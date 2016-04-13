a0c2af8453fe9e9b273c7229a085ba9817da72c8
z_k_w <z_k_w@126.com>
2016-04-13 22:37:36 +0800

    refine server mvp01 api

--- c2s_controller.erl
+++ c2s_controller.erl
@@ -1,5 +1,5 @@
 -module(c2s_controller).
--import(mvp01,[getBalance/0,getMember/1]).
+-import(mvp01,[getBalance/0,getMember/1,register/1,deposite/1,withdraw/1]).
 -import(rfc4627,[encode/1,decode/1]).
 -export([do/3]).
 
@@ -12,8 +12,14 @@ do(SessionID, _Env, Input) ->
 		"getBalance" ->
 			Content = getBalance();
 		"getMember" ->
-			Content = getMember(binary_to_list(Params));
-		true ->
+			Content = getMember(Params);
+		"register" ->
+			Content = register(Params);
+		"deposite" ->
+			Content = deposite(Params);
+		"withdraw" ->
+			Content = withdraw(Params);
+		_ ->
 			Content = "No such query."
 	end,
 	mod_esi:deliver(SessionID, [Header, unicode:characters_to_binary(Content), ""]).
--- mvp01.erl
+++ mvp01.erl
@@ -1,5 +1,5 @@
 -module(mvp01).
--export([getBalance/0,getMember/1,register/2,deposite/2,withdraw/2,readMemberData/1]).
+-export([getBalance/0,getMember/1,register/1,deposite/1,withdraw/1,readMemberData/1]).
 -import(rfc4627,[encode/1,decode/1]).
 -import(apilib,[call/2,eth_getBalance/1,eth_getCompilers/0,eth_compileSolidity/1,eth_sendTransaction/4,eth_getTransactionReceipt/1,web3_sha3/1,padleft/2,get_methodCallData/2,get_methodSignHash/1,eth_methodCall/3,get_methodSign/2,eth_propertyCall/2,eth_propertyMappingCall/3,string2hexstring/1,hexstring2string/1,hex2de/1]).
 -define(CA, "0x6B015e3c7D407977fa053e577F89A319667d3A21").
@@ -7,16 +7,20 @@
 getBalance() ->
 	eth_getBalance("0x01E4Cb51Ec4768B9430b06A6EC2284C7977cCa48").
 
-getMember(Oid) ->
+getMember(Params) ->
+	[Oid|L] = Params,
 	readMemberData(eth_propertyMappingCall(?CA, "memberList", [{"bytes32", Oid, 64, 0}])).
 
-register(Oid, Name) ->
+register(Params) ->
+	[Oid, Amount|L] = Params,
 	eth_methodCall(?CA, "Register", [{"bytes32", Oid, 64, 0}, {"string", Name, 64, 64}]).
 
-deposite(Oid, Amount) ->
+deposite(Params) ->
+	[Oid, Amount|L] = Params,
 	eth_methodCall(?CA, "Deposite", [{"bytes32", Oid, 64, 0}, {"uint256", Amount, 64, 0}]).
 
-withdraw(Oid, Amount) ->
+withdraw(Params) ->
+	[Oid, Amount|L] = Params,
 	eth_methodCall(?CA, "Withdraw", [{"bytes32", Oid, 64, 0}, {"uint256", Amount, 64, 0}]).
 
 readMemberData(Data) ->
