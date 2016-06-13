aa01e4983fa07fb847bef35aa4d44ac9239c941e
david-meshstudio <david@mesh-studio.com>
2016-06-13 13:28:53 +0800

    refine

--- c2s_controller.erl
+++ c2s_controller.erl
@@ -1,5 +1,5 @@
 -module(c2s_controller).
--import(mvp01,[getBalance/0,getMember/1,register/1,deposite/1,withdraw/1,getTranBlockGap/1]).
+-import(mvp01,[getBalance/0,getBalance/1,getMember/1,register/1,deposite/1,withdraw/1,getTranBlockGap/1]).
 -import(rfc4627,[encode/1,decode/1]).
 -export([do/3]).
 
@@ -9,8 +9,10 @@ do(SessionID, _Env, Input) ->
 	Header = ["Content-Type: text/plain; charset=utf-8\r\n\r\n"],
 	{ok, {obj, [{_, Command}, {_, Params}]}, []} = Data,
 	case binary_to_list(Command) of
+		"getBalance" when Params =:= "" ->
+			Content = encode(getBalance());
 		"getBalance" ->
-			Content = getBalance();
+			Content = encode(getBalance(binary_to_list(Params)));
 		"getMember" ->
 			Content = getMember(Params);
 		"register" ->
